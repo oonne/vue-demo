0c185b1f7d60e863cdc692deffb96e1326cf31be
hejiayi <hejiayi@kp99.cn>
2023-07-22 11:17:19 +0800

    feat: 更新依赖

--- src/router/pages.ts
+++ src/router/pages.ts
@@ -1,25 +1,30 @@
 interface Page {
-  path: string;
   pageName: string;
+  path: string;
+  component: string;
 }
 
 /* 路由页面 */
 const pages: Page[] = [
   {
-    path: '/ModuleA/PageA1',
     pageName: 'A1',
+    path: '/ModuleA/PageA1',
+    component: '/ModuleA/PageA1',
   },
   {
-    path: '/ModuleA/PageA2',
     pageName: 'A2',
+    path: '/ModuleA/PageA2',
+    component: '/ModuleA/PageA2',
   },
   {
-    path: '/ModuleB/PageB1',
     pageName: 'B1',
+    path: '/ModuleB/PageB1',
+    component: '/ModuleA/PageA2',
   },
   {
-    path: '/ModuleB/PageB2',
     pageName: 'B2',
+    path: '/ModuleB/PageB2',
+    component: '/ModuleA/PageA2',
   },
 ];
 
