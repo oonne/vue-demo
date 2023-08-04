f8efdaf258ba2f648a5744a0eda766cb7689c20a
hejiayi <hejiayi@kp99.cn>
2023-08-04 17:37:27 +0800

    feat: 背景图

--- src/pages/system/login/index.vue
+++ src/pages/system/login/index.vue
@@ -5,8 +5,15 @@ auth.test1({ a: 'test1' });
 </script>
 
 <template>
-  <div>登录页面TODO</div>
+  <div class="login-bg">
+    登录页面TODO
+  </div>
 </template>
 
 <style scoped>
+.login-bg{
+  min-height: 100vh;
+  background: url('@/assets/imgs/login-bg.png');
+  background-size: cover;
+}
 </style>
