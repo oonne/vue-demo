41aeb5aae8d6239e2f4e8be81f42ddfc8b282b25
github <JAY@oonne.com>
2023-11-04 21:26:40 +0800

    feat: 侧导航

---
+++ src/layout/app-view.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view>
+    <template #default="{ Component, route }">
+      <KeepAlive>
+        <component
+          :is="Component"
+          :key="route.fullPath"
+        />
+      </KeepAlive>
+    </template>
+  </router-view>
+</template>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,22 +1,41 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-    <div>layout</div>
+  <a-layout
+    style="min-height: 100vh"
+  >
+    <!-- 侧导航 -->
+    <a-layout-sider
+      v-model:collapsed="collapsed"
+      theme="light"
+      :trigger="null"
+      collapsible
+    >
+      <SideView />
+    </a-layout-sider>
 
-    <router-view>
-      <template #default="{ Component, route }">
-        <KeepAlive>
-          <component
-            :is="Component"
-            :key="route.fullPath"
-          />
-        </KeepAlive>
-      </template>
-    </router-view>
-  </div>
+    <a-layout>
+      <!-- 顶栏 -->
+      <a-layout-header
+        theme="light"
+      >
+        <HeaderView />
+      </a-layout-header>
+
+      <!-- 内容 -->
+      <a-layout-content>
+        <AppView />
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
-<style>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import AppView from './app-view.vue';
+import HeaderView from './header-view.vue';
+import SideView from './side-view.vue';
+
+const collapsed = ref<boolean>(false);
+</script>
+
+<style scoped>
 </style>
